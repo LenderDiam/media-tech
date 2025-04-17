03f5eded9bdefef1b41d106a73b116b71076316f
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-04-17 17:14:29 +0200

    Add unique constraint for email in User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class User
 {
     #[ORM\Id]

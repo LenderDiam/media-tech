a8f4f59524144d795d9c4a94fde3c2bc161c2fa4
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-04-24 18:13:57 +0200

    Add label

--- src/Enum/MembershipState.php
+++ src/Enum/MembershipState.php
@@ -7,4 +7,13 @@ enum MembershipState: int
     case Pending = 1;
     case Approved = 2;
     case Rejected = 3;
+
+    public function label(): string
+    {
+        return match($this) {
+            self::Pending => 'en attente',
+            self::Approved => 'approuvé',
+            self::Rejected => 'rejeté',
+        };
+    }
 }
